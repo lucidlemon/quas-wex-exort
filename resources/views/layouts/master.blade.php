56471c7ec3a8ae18948a7d55328939d9e9e3366b
Daniel Winter <d.kreide@gmail.com>
2016-12-05 10:45:09 +0100

    better active links on guides

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -84,7 +84,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="{{ url('/guides') }}" class="{{ active('guides') }}">
+                        <a href="{{ url('/guides') }}" class="{{ active(['guides', 'guides/heroes', 'guides/items', 'guides/post']) }}">
                             <img src="{{asset('assets/ic_guides.svg')}}" />
                             Guides
                         </a>
