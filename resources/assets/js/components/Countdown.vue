a22328af8b3367460bcaacc7c9718732e1f7b111
Daniel Winter <d.kreide@gmail.com>
2017-01-25 20:14:42 +0100

    producing less output i guess

--- resources/assets/js/components/Countdown.vue
+++ resources/assets/js/components/Countdown.vue
@@ -66,7 +66,7 @@
     },
     methods: {
       getTimeRemaining(endtime) {
-        const total = Date.parse(endtime) - Date.parse(new Date());
+        const total = endtime.diff(moment());
         const seconds = Math.floor((total / 1000) % 60);
         const minutes = Math.floor((total / 1000 / 60) % 60);
         const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
