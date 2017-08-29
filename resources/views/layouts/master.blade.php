318ac6f34b4517122c4914de7ff8e77d38d62791
Daniel Winter <hallo@danielwinter.at>
2017-08-29 11:17:38 +0200

    updating layout settings to use laravel-mix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -62,7 +62,7 @@
         @if(env('APP_DEBUG', false))
             <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         @else
-            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+            <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         @endif
 
     </head>
@@ -158,7 +158,7 @@
         @if(env('APP_DEBUG', false))
             <script src="{{ asset('js/app.js') }}"></script>
         @else
-            <script src="{{ asset('js/app.js') }}"></script>
+            <script src="{{ mix('js/app.js') }}"></script>
 
             <script>
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
