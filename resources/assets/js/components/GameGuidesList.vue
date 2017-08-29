dfd34cfc3c621bf0dcc1c8d49ac69a86bd632a51
Daniel Winter <hallo@danielwinter.at>
2017-08-29 12:44:03 +0200

    possible fix for guide list bug

--- resources/assets/js/components/GameGuidesList.vue
+++ resources/assets/js/components/GameGuidesList.vue
@@ -22,8 +22,10 @@
           <img class="guide-image" :src="parent.image" />
         </div>
         <div class="col-xs-9 col-sm-10 col-md-8" >
-          <h3 v-if="parent.localized_name">{{ parent.localized_name }}</h3>
-          <h3 v-if="parent.title">{{ parent.title }}</h3>
+          <div v-if="parent">
+            <h3 v-if="parent.localized_name">{{ parent.localized_name }}</h3>
+            <h3 v-if="parent.title">{{ parent.title }}</h3>
+          </div>
 
           <div class="guide-links">
             <div class="guide-link" v-for="guide in parent.guides">
